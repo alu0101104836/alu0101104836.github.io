69b5d836926db9de14700981a82b5ec57d4a860c
Carlos Socas García <alu0101104836@ull.edu.es>
2019-12-09 17:25:17 +0000

    Empezando listas

--- spec/plato_spec.rb
+++ spec/plato_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Plato do
       @alimento1 = Alimentos.new("Carne de Vaca",21.1,0.0,3.1,50.0,164.0)
       @alimento2 = Alimentos.new("Huevos",13.0,1.1,11.0,4.2,5.7)
 
+      @lista_1 = Lista.new()
     end
 
     context "Comparar alimentos" do
@@ -18,6 +19,16 @@ RSpec.describe Plato do
     end
 
     context "Listas" do
+      it "Collect" do
+        @lista_1.insert_head(@alimento1)
+        @lista_1.insert_head(@alimento2)
+        expect(@lista_1.collect{|i| i.to_s}).to eq(["(Huevos,13.0,1.1,11.0,4.2,5.7)", "(Carne de Vaca,21.1,0.0,3.1,50.0,164.0)"])        
+      end
+
+      it "Select" do
+        
+      end
+      
       
     end
     
